b90225bece16a49270c911f6d99675bfa55f4268
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2018-03-15 12:07:56 -0400

    Updated directory

--- recipes/market-basket-post-install.sh
+++ recipes/market-basket-post-install.sh
@@ -315,7 +315,7 @@ handleGroupPorts (){
 
 createHDFSDirectories () {
   unzip /root/CloudBreakArtifacts/recipes/MARKET_BASKET_DEMO_CONTROL/demofiles/OnlineRetail.txt.zip
-  cp OnlineRetail.txt /tmp/
+  cp /root/CloudBreakArtifacts/recipes/MARKET_BASKET_DEMO_CONTROL/demofiles/OnlineRetail.txt /tmp/
   sudo -u hdfs hadoop fs -mkdir /user/root
   sudo -u hdfs hadoop fs -mkdir /user/root/retail
   sudo -u hdfs hadoop fs -mkdir /user/root/retail/retailsalesraw
